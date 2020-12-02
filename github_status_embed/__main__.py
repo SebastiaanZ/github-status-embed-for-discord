6dbdd9f87c655d58414e42a3c8a0524548cab43a
Sebastiaan Zeeff <33516116+SebastiaanZ@users.noreply.github.com>
2020-12-02 22:07:55 +0100

    Try importing installed dependency

--- github_status_embed/__main__.py
+++ github_status_embed/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import requests
 
 parser = argparse.ArgumentParser(
     description="Send a custom GitHub Actions Status Embed to a Discord webhook.",
@@ -10,4 +11,5 @@ parser.add_argument("name")
 if __name__ == "__main__":
     args = parser.parse_args()
     print(f"Hello, {args.name}!")
+    print(requests.__version__)
     print(f"::set-output name=status::ok")
