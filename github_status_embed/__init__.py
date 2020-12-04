64dd32b1461e2b4074fe2277165eb908bc8c8d04
Sebastiaan Zeeff <sebastiaan.zeeff@gmail.com>
2020-12-04 15:30:57 +0100

    Add package description to __init__ docstring
    I've added a short description of the package to the __init__.py's
    docstring. While the repository will also have a README, this may come
    in handy if a future version is packaged and pushed to PyPI.

---
+++ github_status_embed/__init__.py
@@ -0,0 +1,9 @@
+"""
+Github Status Embed for Discord.
+
+This applications sends an enhanced GitHub Actions Status Embed to a
+Discord webhook. The default embeds sent by GitHub don't contain a lot
+of information and are send for every workflow run. This application,
+part of a Docker-based action, allows you to send more meaningful embeds
+when you want to send them.
+"""
