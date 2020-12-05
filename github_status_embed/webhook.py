aae1bfea528113a401f4e8579ae12b3260c519c7
Sebastiaan Zeeff <sebastiaan.zeeff@gmail.com>
2020-12-05 11:35:31 +0100

    Shorten embed title by removing workflow name
     The embeds generated for general workflow runs contained the name of
     the workflow at two separate places: in the title and in one of the
     fields.
    
     This redundancy increased the chance of the title getting split over
     two separate lines, causing the embed to take up more vertical space
     than strictly needed.
    
     The resolution was to remove the workflow name from the title.

--- github_status_embed/webhook.py
+++ github_status_embed/webhook.py
@@ -85,10 +85,7 @@ def get_payload(workflow: types.Workflow) -> types.WebhookPayload:
     ]
 
     embed = types.Embed(
-        title=(
-            f"[{workflow.repository}] Workflow {workflow.status.adjective}: "
-            f"{workflow.name} #{workflow.number}"
-        ),
+        title=f"[{workflow.repository}] Workflow {workflow.status.adjective}",
         description=EMBED_DESCRIPTION.format(
             run_id=workflow.id, run_url=workflow.url, status_verb=workflow.status.verb,
         ),
